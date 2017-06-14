cdec8b1f0c59af700de44af8727334d938a4166a
Jake Meyer <jakewmeyer@gmail.com>
2017-06-14 15:06:01 -0500

    added exception handling and exit options

--- movie_search/movie.rb
+++ movie_search/movie.rb
@@ -3,34 +3,57 @@
 
 require 'Rest-Client'
 require 'json'
-require 'word_wrap'
+
 
 # Word wrapping method
 def wrap(s, width=78)
   s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n| ")
 end
 
+# Method for movie searching logic
 def movie_search
 
-  api_key = "1234567"
-
+  # Captures user input
+  puts ""
   print "Movie =>  "
   movie_name = gets.chomp
-  url = "http://www.omdbapi.com/?t=#{movie_name}&apikey=#{api_key}"
+
+  # Program escape statements
+  if movie_name == "quit" || movie_name == "exit"
+      puts ""
+      exit(0)
+  else
+
+  # API fetch for movie
+  url = "http://www.omdbapi.com/?t=#{movie_name}&apikey=946f500a"
   response = RestClient.get(url)
   info = JSON.parse(response)
 
+  # Exception for invalid response
+  if info["Response"] == "False"
+      puts ""
+      puts "No Movie Found"
+      puts ""
+      exit(0)
+  else
+
+  # Rescue if no tomato score
+  # Word wrap added to plot and actors
+  begin
   title = info["Title"]
   year = info["Year"]
   score = info["Ratings"][1]["Value"]
+  rescue
+      score = "No Score Found"
+  end
   rated = info["Rated"]
   genre = info["Genre"]
   director = info["Director"]
   actors = wrap(info["Actors"], 48)
   plot_unformatted = info["Plot"]
-
   plot = wrap(plot_unformatted, 48)
 
+  # Format for printing to screen
   puts ""
   puts "=================================================="
   puts "| Title: #{title}"
@@ -43,6 +66,8 @@ def movie_search
   puts "| Plot: #{plot}"
   puts "=================================================="
   puts ""
+  end
+  end
 end
 
 movie_search
