e82acaa200d75104c4dfb9046b8787d477e53290
Jake Meyer <jakewmeyer@gmail.com>
2017-06-14 21:37:24 -0500

    added error handling to weather script

--- weather/weather.rb
+++ weather/weather.rb
@@ -5,17 +5,22 @@ require 'rest-client'
 require 'json'
 
 def weather_search
-
-    # This key is functional, don't abuse it
+    # This key is functional, please don't abuse it
     api_key = "6510b92495fd472ca30155709172803&q"
 
     # Uses IP to get current city
+    begin
     url = "http://ip-api.com/json"
     response = RestClient.get(url)
     parsed = JSON.parse(response)
     location = parsed["city"]
+    rescue
+        puts "No IP Response"
+        exit(0)
+    end
 
     # Uses city to fetch weather
+    begin
     url = "https://api.apixu.com/v1/current.json?key=#{api_key}=#{location}"
     response = RestClient.get(url)
     parsed = JSON.parse(response)
@@ -27,6 +32,10 @@ def weather_search
     humidity = parsed["current"]["humidity"]
     feels_like = parsed["current"]["feelslike_f"]
     visability = parsed["current"]["vis_miles"]
+    rescue
+        puts "No Weather API Response"
+        exit(0)
+    end
 
     puts ""
     puts "======================"
