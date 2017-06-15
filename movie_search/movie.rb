6e6c8158a199b357337c53008c2739c43d3f857a
Jake Meyer <jakewmeyer@gmail.com>
2017-06-14 23:04:04 -0500

    replaced double quotes with single

--- movie_search/movie.rb
+++ movie_search/movie.rb
@@ -5,22 +5,22 @@ require 'Rest-Client'
 require 'json'
 
 # Word wrapping method
-def wrap(s, width=78)
+def wrap(s, width = 78)
   s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n| ")
 end
 
-def movie_search
+def movie
   # This is a live API key, don't absue it
-  api_key = "946f500a"
+  api_key = '946f500a'
 
   # Captures user input
-  puts ""
-  print "Movie =>  "
+  puts ''
+  print 'Movie =>  '
   movie_name = gets.chomp
 
   # Program escape statements
-  if movie_name == "quit" || movie_name == "exit"
-    puts ""
+  if movie_name == 'quit' || movie_name == 'exit'
+    puts ''
     exit(0)
   else
     # API fetch for movie
@@ -30,32 +30,32 @@ def movie_search
   end
 
   # Exception for invalid response
-  if info["Response"] == "False"
-    puts ""
-    puts "No Movie Found"
-    puts ""
+  if info['Response'] == 'False'
+    puts ''
+    puts 'No Movie Found'
+    puts ''
     exit(0)
   else
     # Rescue if no tomato score
     # Word wrap added to plot and actors
     begin
-      title = info["Title"]
-      year = info["Year"]
-      score = info["Ratings"][1]["Value"]
+      title = info['Title']
+      year = info['Year']
+      score = info['Ratings'][1]['Value']
     rescue
-      score = "No Score Found"
+      score = 'No Score Found'
     end
 
-    rated = info["Rated"]
-    genre = info["Genre"]
-    director = info["Director"]
-    actors = wrap(info["Actors"], 48)
-    plot_unformatted = info["Plot"]
+    rated = info['Rated']
+    genre = info['Genre']
+    director = info['Director']
+    actors = wrap(info['Actors'], 48)
+    plot_unformatted = info['Plot']
     plot = wrap(plot_unformatted, 48)
 
     # Format for printing to screen
-    puts ""
-    puts "=================================================="
+    puts ''
+    puts '=================================================='
     puts "| Title: #{title}"
     puts "| Year: #{year}"
     puts "| Tomato: #{score}"
@@ -64,9 +64,9 @@ def movie_search
     puts "| Director: #{director}"
     puts "| Actors: #{actors}"
     puts "| Plot: #{plot}"
-    puts "=================================================="
-    puts ""
+    puts '=================================================='
+    puts ''
   end
 end
 
-movie_search
+movie
