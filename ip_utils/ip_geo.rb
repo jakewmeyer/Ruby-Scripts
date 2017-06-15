cdbe235692d36450448b7e28f058a0ade5bc82d4
Jake Meyer <jakewmeyer@gmail.com>
2017-06-15 12:21:34 -0500

    added escape words

--- ip_utils/ip_geo.rb
+++ ip_utils/ip_geo.rb
@@ -9,40 +9,46 @@ def ip_location
   print 'IP => '
   ip = gets.chomp
 
-  begin
-    url = "http://ip-api.com/json/#{ip}"
-    response = RestClient.get(url)
-    info = JSON.parse(response)
-  rescue
-    puts ''
-    puts 'IP not found.'
-    puts ''
-    exit(0)
-  end
-
-  ip = info['query']
-  city = info['city']
-  region = info['region']
-  country = info['country']
-  isp = info['isp']
-  zip = info['zip']
-
-  if city.nil?
-    puts ''
-    puts 'IP not found.'
+  if ip == 'quit' || ip == 'exit'
     puts ''
     exit(0)
   else
-    puts ''
-    puts '============================='
-    puts "| IP: #{ip}"
-    puts "| City: #{city}"
-    puts "| Region: #{region}"
-    puts "| Country: #{country}"
-    puts "| ZIP: #{zip}"
-    puts "| ISP: #{isp}"
-    puts '============================='
-    puts ''
+    
+    begin
+      url = "http://ip-api.com/json/#{ip}"
+      response = RestClient.get(url)
+      info = JSON.parse(response)
+    rescue
+      puts ''
+      puts 'IP not found.'
+      puts ''
+      exit(0)
+    end
+
+    ip = info['query']
+    city = info['city']
+    region = info['region']
+    country = info['country']
+    isp = info['isp']
+    zip = info['zip']
+
+    if city.nil?
+      puts ''
+      puts 'IP not found.'
+      puts ''
+      exit(0)
+    else
+      puts ''
+      puts '============================='
+      puts "| IP: #{ip}"
+      puts "| City: #{city}"
+      puts "| Region: #{region}"
+      puts "| Country: #{country}"
+      puts "| ZIP: #{zip}"
+      puts "| ISP: #{isp}"
+      puts '============================='
+      puts ''
+    end
   end
 end
 
