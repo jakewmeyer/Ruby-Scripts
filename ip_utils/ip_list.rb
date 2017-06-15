541e168a7171ee50a8fbfd5fb65b54181bce3c45
Jake Meyer <jakewmeyer@gmail.com>
2017-06-14 22:11:18 -0500

    exceptions for DNS and Router

--- ip_utils/ip_list.rb
+++ ip_utils/ip_list.rb
@@ -5,15 +5,15 @@
 require 'Rest-Client'
 require 'Socket'
 
-def network_info()
+def network_info
 
   # Grabs public WAN address
   begin
   url = "https://api.ipify.org"
   response = RestClient.get(url)
   rescue
-      puts "Can't find WAN"
-      exit(0)
+    puts "Can't find WAN"
+    exit(0)
   end
 
   # Grabs assigned IP and formats it
@@ -21,21 +21,34 @@ def network_info()
   ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
   lan = ip.ip_address
   rescue
-      puts "Cant find LAN"
-      exit(0)
+    puts "Cant find LAN"
+    exit(0)
   end
 
-  # Parses netstat and scutil for router and DNS
+  # Greps scutil for DNS server
+  begin
   dns = %x[scutil --dns | grep nameserver | head -1 | awk '{print$3}']
-  router = %x[netstat -rn | grep default | head -1 | awk '{print$2}']
+  rescue
+    puts "Can't find DNS"
+    exit(0)
+  end
 
+  # Greps netstat for router address
+  begin
+  router = %x[netstat -rn | grep default | head -1 | awk '{print$2}']
+  rescue
+    puts "Can't find Router"
+    exit(0)
+  end
+
+  # Formatting
   puts ""
-  puts "====================="
+  puts "======================"
   puts "| WAN: #{response}"
   puts "| LAN: #{lan}"
   puts "| ROUTER: #{router}"
   puts "| DNS: #{dns}"
-  puts "====================="
+  puts "======================"
   puts ""
 end
 
