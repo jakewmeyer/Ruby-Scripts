5327629615c6b4a5202f361625b0ef8570baf391
Jake Meyer <jakewmeyer@gmail.com>
2017-06-14 23:20:26 -0500

    replaced double quotes with single

--- stock_search/stocks.rb
+++ stock_search/stocks.rb
@@ -7,11 +7,11 @@ require 'json'
 
 def stock_search
     # Cptures user inputs
-    puts ""
-    print "Stock => "
+    puts ''
+    print 'Stock => '
     symbol = gets.chomp
 
-    if symbol == "quit" || symbol == "exit"
+    if symbol == 'quit' || symbol == 'exit'
         exit(0)
     else
 
@@ -20,11 +20,11 @@ def stock_search
     url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=#{symbol}&region=1&lang=en%22"
     response = RestClient.get(url)
     parsed = JSON.parse(response)
-    parsed_symbol = parsed["ResultSet"]["Result"][0]["symbol"]
+    parsed_symbol = parsed['ResultSet']['Result'][0]['symbol']
     rescue
-        puts ""
-        puts "Invalid Search"
-        puts ""
+        puts ''
+        puts 'Invalid Search'
+        puts ''
         exit(0)
     end
     # Rest API to fetch current JSON data
@@ -32,25 +32,25 @@ def stock_search
     response = RestClient.get(url)
     parsed = JSON.parse(response)
 
-    if parsed["Realtime Global Securities Quote"].empty?
-        puts ""
-        puts "No Stock Found"
-        puts ""
+    if parsed['Realtime Global Securities Quote'].empty?
+        puts ''
+        puts 'No Stock Found'
+        puts ''
         exit(0)
     else
         # Assigning variables to hash key values
-        parsed_symbol = parsed["Realtime Global Securities Quote"]["01. Symbol"]
-        parsed_price = parsed["Realtime Global Securities Quote"]["03. Latest Price"]
-        parsed_open = parsed["Realtime Global Securities Quote"]["04. Open (Current Trading Day)"]
-        parsed_high = parsed["Realtime Global Securities Quote"]["05. High (Current Trading Day)"]
-        parsed_price_chg = parsed["Realtime Global Securities Quote"]["08. Price Change"]
-        parsed_price_pct = parsed["Realtime Global Securities Quote"]["09. Price Change Percentage"]
-        parsed_volume = parsed["Realtime Global Securities Quote"]["10. Volume (Current Trading Day)"]
-        parsed_exchange = parsed["Realtime Global Securities Quote"]["02. Exchange Name"]
+        parsed_symbol = parsed['Realtime Global Securities Quote']['01. Symbol']
+        parsed_price = parsed['Realtime Global Securities Quote']['03. Latest Price']
+        parsed_open = parsed['Realtime Global Securities Quote']['04. Open (Current Trading Day)']
+        parsed_high = parsed['Realtime Global Securities Quote']['05. High (Current Trading Day)']
+        parsed_price_chg = parsed['Realtime Global Securities Quote']['08. Price Change']
+        parsed_price_pct = parsed['Realtime Global Securities Quote']['09. Price Change Percentage']
+        parsed_volume = parsed['Realtime Global Securities Quote']['10. Volume (Current Trading Day)']
+        parsed_exchange = parsed['Realtime Global Securities Quote']['02. Exchange Name']
 
         # Output of parsed hash
-        puts ""
-        puts "======================"
+        puts ''
+        puts '======================'
         puts "| Symbol: #{parsed_symbol}"
         puts "| Price: $#{parsed_price}"
         puts "| Open: $#{parsed_open}"
@@ -59,8 +59,8 @@ def stock_search
         puts "| Price Chg % : #{parsed_price_pct}"
         puts "| Volume: #{parsed_volume}"
         puts "| Echange: #{parsed_exchange}"
-        puts "======================"
-        puts ""
+        puts '======================'
+        puts ''
     end
     end
 end
