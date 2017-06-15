4bf2362f8df8981d20d3a940d27e3e979260b1c4
Jake Meyer <jakewmeyer@gmail.com>
2017-06-15 10:51:53 -0500

    replaced double quotes with single + exception handling

--- ip_utils/ip_geo.rb
+++ ip_utils/ip_geo.rb
@@ -4,27 +4,34 @@
 require 'json'
 require 'rest-client'
 
-def ip_location()
-  print "What's the  IP? => "
+def ip_location
+  puts ''
+  print 'IP => '
   ip = gets.chomp
 
-  url = "http://ipinfo.io/#{ip}/json"
-  response = RestClient.get(url)
-  info = JSON.parse(response)
+  begin
+    url = "http://ipinfo.io/#{ip}/json"
+    response = RestClient.get(url)
+    info = JSON.parse(response)
+  rescue
+    puts ''
+    puts 'IP not found.'
+    puts ''
+    exit(0)
+  end
+
+  city = info['city']
+  region = info['region']
+  country = info['country']
+  hostname = info['hostname']
 
 
-  city = info["city"]
-  region = info["region"]
-  country = info["country"]
-  hostname = info["hostname"]
-
-
-  puts ""
+  puts ''
   puts "City: #{city}"
   puts "Region: #{region}"
   puts "Country: #{country}"
   puts "Host: #{hostname}"
-  puts ""
+  puts ''
 end
 
-ip_location()
+ip_location
