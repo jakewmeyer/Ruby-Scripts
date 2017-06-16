6c56b464c63895933c8317ed2febcdcb298b7517
Jake Meyer <jakewmeyer@gmail.com>
2017-06-16 15:56:58 -0500

    defined user inputs, BigDecimal, and added top comments

--- currency/currency.rb
+++ currency/currency.rb
@@ -1,5 +1,28 @@
 #!/usr/bin/env ruby
+
 # CLI app for currency conversion
+# Uses BigDecimal for accuracy
+# Uses Fixer.io as data source
 
 require 'rest-client'
 require 'json'
+require 'bigdecimal'
+
+def user_input
+  print "Base - Ex.'USD' => "
+  base_currency = gets.chomp
+  print "Amount - Ex.'13.76' => "
+  initial_amount = gets.chomp
+  print "Convert to - Ex.'EUR'"
+  convert = gets.chomp
+end
+
+
+def api_fetch
+
+end
+
+
+def conversion
+
+end
