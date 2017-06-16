f630daae2f3945b9b28018f51be0e8ea01e225df
Jake Meyer <jakewmeyer@gmail.com>
2017-06-16 18:05:51 -0500

    fixed locale bug, working properly

--- currency/currency.rb
+++ currency/currency.rb
@@ -9,32 +9,35 @@ require 'json'
 require 'money'
 require 'monetize'
 
-# User input
-print "Base - Ex.'USD' => "
-base_currency = gets.chomp.upcase
-print "Amount - Ex.'13.76' => "
-initial_amount = gets.chomp
-print "Convert to - Ex.'EUR' => "
-convert_to = gets.chomp.upcase
+def conversion
+  # User input
+  print "Base - Ex.'USD' => "
+  base_currency = gets.chomp.upcase
+  print "Amount - Ex.'13.76' => "
+  initial_amount = gets.chomp
+  print "Convert to - Ex.'EUR' => "
+  convert_to = gets.chomp.upcase
 
-# Fetches API info
-url = "http://api.fixer.io/latest?base=#{base_currency}&symbols=#{convert_to}"
-response = RestClient.get(url)
-parsed = JSON.parse(response)
-convert_factor = (parsed['rates'][convert_to])
+  # Fetches API info
+  url = "http://api.fixer.io/latest?base=#{base_currency}&symbols=#{convert_to}"
+  response = RestClient.get(url)
+  parsed = JSON.parse(response)
+  convert_factor = (parsed['rates'][convert_to])
 
-# Conversion setup and logic
-Money.add_rate(base_currency, convert_to, convert_factor)
+  # Fixes local exception bug
+  I18n.enforce_available_locales = false
 
-initial = Monetize.parse("#{base_currency} #{initial_amount}")
+  # Conversion setup and logic
+  Money.add_rate(base_currency, convert_to, convert_factor)
+  initial = Monetize.parse("#{base_currency} #{initial_amount}")
+  final_convert = initial.exchange_to(convert_to)
 
-final_convert = initial.exchange_to(convert_to)
-
-# Output formatting
-puts ''
-puts '================='
-puts "| #{base_currency} to #{convert_to}"
-puts "| #{base_currency}: #{initial}"
-puts "| #{convert_to}: #{final_convert}"
-puts '================='
-puts ''
+  # Output formatting
+  puts ''
+  puts '================'
+  puts "| #{base_currency} to #{convert_to}"
+  puts "| #{base_currency}: #{initial.format}"
+  puts "| #{convert_to}: #{final_convert.format}"
+  puts '================'
+  puts ''
+end
