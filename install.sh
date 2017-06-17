7d6ca86a7318efd5d84a4f30053d146d8ac07ec1
Jake Meyer <jakewmeyer@gmail.com>
2017-06-16 19:23:43 -0500

    removed spacing in instal.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@
 # it downloads the files, and installs all
 # the gems necessary to run them
 
-echo ""
 echo "Installing"
-echo ""
 gem install rest-client
 gem install money
 gem install monetize
-echo ""
 echo "Finished gem install"
-echo ""
