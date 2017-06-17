1431027f1cca54127aeb31dfd96e4b37cd7fa87e
Jake Meyer <jakewmeyer@gmail.com>
2017-06-16 19:12:58 -0500

    added gems to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-#
+
 # This is the install for my ruby scripts,
 # it downloads the files, and installs all
 # the gems necessary to run them
-#
 
 echo "Installing"
+
+gem install rest-client
+gem install money
+gem install monetize
+
+echo "Finished gem install"
