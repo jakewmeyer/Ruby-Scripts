af620ff53670b6a42c4d49b9d464d7ab87175378
Jake Meyer <jakewmeyer@gmail.com>
2017-06-18 21:47:40 -0500

    added sucess messeges to end of program

--- encryption/encrypt.rb
+++ encryption/encrypt.rb
@@ -10,6 +10,7 @@ def encrypt
   input_file = ARGV[0]
   output_file = ARGV[1]
   %x[openssl enc -aes-256-cbc -salt -a -in #{input_file} -out #{output_file}]
+  puts "Sucessfully encrypted"
 end
 
 encrypt
