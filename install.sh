c3b9e969d86ac0e92d5572802bc3df3599da3267
Jake Meyer <jakewmeyer@gmail.com>
2017-06-18 22:53:17 -0500

    added lots of spacing for readability

--- install.sh
+++ install.sh
@@ -8,12 +8,15 @@
 echo " => Installing"
 echo ''
 gem install rest-client
+echo ''
 gem install money
+echo ''
 gem install monetize
 echo ''
 echo "=> Finished gem install"
-
+echo ''
 curl -LO https://github.com/jakewmeyer/Ruby-Scripts/archive/master.zip
-
+echo ''
 echo "=> Finished file download"
+echo ''
 exit 0
