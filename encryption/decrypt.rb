1eae9bedd46ecd98092da2812e5ef7071d4296b0
Jake Meyer <jakewmeyer@gmail.com>
2017-06-18 21:10:27 -0500

    spliiting to two seperate files

---
+++ encryption/decrypt.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+# Decrypts files easily using
+# openssl commands. Essentially
+# an easy wrapper for openssl
---
+++ encryption/encrypt.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Encrypts files easily using
+# openssl commands. Essentially
+# an easy wrapper for openssl
+
+# Ex. $ encrypt names.txt encrypted.txt
+
+def encrypt
+  input_file = ARGV[0]
+  output_file = ARGV[1]
+  %x[openssl enc -aes-256-cbc -salt -a -in #{input_file} -out #{output_file}]
+end
