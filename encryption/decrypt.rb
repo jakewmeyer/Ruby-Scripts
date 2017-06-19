21b31a0c6af5fc4c6ce968aa79b55030b68bbb38
Jake Meyer <jakewmeyer@gmail.com>
2017-06-18 22:17:55 -0500

    removed shell script error

--- encryption/decrypt.rb
+++ encryption/decrypt.rb
@@ -9,7 +9,7 @@
 def decrypt
   input_file = ARGV[0]
   output_file = ARGV[1]
-  unless system("%x[openssl enc -aes-256-cbc -d -a -in #{input_file} -out #{output_file}")
+  unless system("openssl enc -aes-256-cbc -d -a -in #{input_file} -out #{output_file}")
     puts "Files not found!"
   end
   puts 'Sucessfully decrypted'
