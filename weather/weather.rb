5938b5fccaa63fc473bd8d4dd73c591aba7268ab
Jake Meyer <jakewmeyer@gmail.com>
2018-02-20 11:40:46 -0600

    weather command refactor

--- weather/weather.rb
+++ weather/weather.rb
@@ -1,77 +1,79 @@
 #!/usr/bin/env ruby
 # CLI app that brings in current location weather.
 
-require 'rest-client'
 require 'json'
+require 'uri'
+require 'net/http'
 
-def weather_search
-  # This key is functional, please don't abuse it
+def weather
   api_key = ENV['APIXU_API_KEY'] || "6510b92495fd472ca30155709172803&q"
 
-  # Uses IP to get current city
-  begin
-    url = "http://ip-api.com/json"
-    response = RestClient.get(url)
-    parsed = JSON.parse(response)
-    location = ARGV || parsed["city"]
-  rescue RestClient::BadRequest
-    puts "No IP address found"
-    exit(1)
+  ip = get_ip
+  city = ARGV[0] || ip["city"]
+  state = ARGV[1] || ip["region"]
+  country = ip["countryCode"]
+
+  weather = get_weather(api_key, city, state)
+
+  if country == "US"
+    us_printer(weather)
+  else
+    intl_printer(weather)
   end
-
-  # Uses city to fetch weather
-    begin
-      url = "https://api.apixu.com/v1/current.json?key=#{api_key}=#{location}"
-      response = RestClient.get(url)
-    rescue RestClient::BadRequest
-      puts "Location not found"
-      exit(1)
-    end
-    parsed = JSON.parse(response)
-
-    country = parsed["location"]["country"]
-
-    if country == "United States of America"
-      # Assigning values to variables
-      location_name = parsed["location"]["name"]
-      temp = parsed["current"]["temp_f"]
-      wind_speed = parsed["current"]["wind_mph"]
-      humidity = parsed["current"]["humidity"]
-      feels_like = parsed["current"]["feelslike_f"]
-      visibility = parsed["current"]["vis_miles"]
-
-      # Output for United States
-      puts
-      puts "======================"
-      puts "| City: #{location_name}"
-      puts "| Temp: #{temp}°F"
-      puts "| Feels Like: #{feels_like}°F"
-      puts "| Humidity: #{humidity}%"
-      puts "| Wind Speed: #{wind_speed} mph"
-      puts "| Visibility: #{visibility} mi"
-      puts "======================"
-      puts
-    else
-      # Assigning values to variables
-      location_name = parsed["location"]["name"]
-      temp = parsed["current"]["temp_c"]
-      wind_speed = parsed["current"]["wind_kph"]
-      humidity = parsed["current"]["humidity"]
-      feels_like = parsed["current"]["feelslike_c"]
-      visibility = parsed["current"]["vis_km"]
-
-      # Output for Metric countries
-      puts
-      puts "======================"
-      puts "| City: #{location_name}"
-      puts "| Temp: #{temp}°C"
-      puts "| Feels Like: #{feels_like}°C"
-      puts "| Humidity: #{humidity}%"
-      puts "| Wind Speed: #{wind_speed} kph"
-      puts "| Visibility: #{visibility} km"
-      puts "======================"
-      puts
-    end
 end
 
-weather_search
+def get_ip
+  uri = URI("http://ip-api.com/json")
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+end
+
+def get_weather(api_key, city, state)
+    uri = URI("https://api.apixu.com/v1/current.json?key=#{api_key}=#{city}+#{state}")
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+end
+
+def us_printer(parsed)
+  # Printer for US users
+  location_name = parsed["location"]["name"]
+  temp = parsed["current"]["temp_f"]
+  wind_speed = parsed["current"]["wind_mph"]
+  humidity = parsed["current"]["humidity"]
+  feels_like = parsed["current"]["feelslike_f"]
+  visibility = parsed["current"]["vis_miles"]
+
+  puts
+  puts "======================"
+  puts "City: #{location_name}"
+  puts "Temp: #{temp}°F"
+  puts "Feels Like: #{feels_like}°F"
+  puts "Humidity: #{humidity}%"
+  puts "Wind Speed: #{wind_speed} mph"
+  puts "Visibility: #{visibility} mi"
+  puts "======================"
+  puts
+end
+
+def intl_printer(parsed)
+  # Printer for metric users
+  location_name = parsed["location"]["name"]
+  temp = parsed["current"]["temp_c"]
+  wind_speed = parsed["current"]["wind_kph"]
+  humidity = parsed["current"]["humidity"]
+  feels_like = parsed["current"]["feelslike_c"]
+  visibility = parsed["current"]["vis_km"]
+
+  puts
+  puts "======================"
+  puts "City: #{location_name}"
+  puts "Temp: #{temp}°C"
+  puts "Feels Like: #{feels_like}°C"
+  puts "Humidity: #{humidity}%"
+  puts "Wind Speed: #{wind_speed} kph"
+  puts "Visibility: #{visibility} km"
+  puts "======================"
+  puts
+end
+
+weather
