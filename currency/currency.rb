9021d069949c3b539e3b3d766ae730e7469dee64
Jake Meyer <jakewmeyer@gmail.com>
2018-02-22 08:46:42 -0600

    added currency argv data

--- currency/currency.rb
+++ currency/currency.rb
@@ -13,13 +13,14 @@ require 'monetize'
 
 def conversion
   # User input
-  puts
-  print "Base => "
-  base_currency = gets.chomp.upcase
-  print "Amount => "
-  initial_amount = gets.chomp
-  print "Convert to => "
-  convert_to = gets.chomp.upcase
+  begin
+    base_currency = ARGV[0].upcase
+    initial_amount = ARGV[1]
+    convert_to = ARGV[2].upcase
+  rescue
+    puts 'Input Error'
+    exit(1)
+  end
 
   # Fetches API info
   begin
@@ -38,18 +39,23 @@ def conversion
   I18n.enforce_available_locales = false
 
   # Creates money exchange rate and new money object
-  Money.add_rate(base_currency, convert_to, convert_factor)
+  begin
+    Money.add_rate(base_currency, convert_to, convert_factor)
+  rescue Money::Currency::UnknownCurrency
+    puts 'Invalid Currency'
+    exit(1)
+  end
   initial = Monetize.parse("#{base_currency} #{initial_amount}")
   final_convert = initial.exchange_to(convert_to)
 
   # Output formatting
   puts
-  puts '================'
-  puts "| #{base_currency} to #{convert_to}"
-  puts "| RATE: #{convert_factor}"
-  puts "| #{base_currency}: #{initial.format}"
-  puts "| #{convert_to}: #{final_convert.format}"
-  puts '================'
+  puts '================='
+  puts "#{base_currency} to #{convert_to}"
+  puts "Rate: #{convert_factor}"
+  puts "#{base_currency}: #{initial.format}"
+  puts "#{convert_to}: #{final_convert.format}"
+  puts '================='
   puts
 end
 
